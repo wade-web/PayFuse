500349804c84e5a743df832060b44ce3991c4cbb
wade-web <freedevweb221@gmail.com>
2025-06-30 10:54:58 +0200

    Update StatsCard.tsx

--- src/components/StatsCard.tsx
+++ src/components/StatsCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { motion } from 'framer-motion'
-import { DivideIcon as LucideIcon } from 'lucide-react'
+import { LucideIcon } from 'lucide-react'
 
 interface StatsCardProps {
   title: string
